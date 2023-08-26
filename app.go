4ddcac71c2f804c556e34ed7f62892ed5ba4b8b9
Hector <hhernandez27mx@gmail.com>
2023-08-26 11:53:53 -0600

    con gin

--- app.go
+++ app.go
@@ -1,22 +1,13 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-
-	"azt.com/api-sample/infra/controller"
+	"azt.com/api-sample/infra/router"
 )
 
 func init() {
 
 }
 func main() {
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Página de inicio")
-	})
-	http.HandleFunc("/contact", controller.HomeContact)
-	http.HandleFunc("/contact/add", controller.AdddContactHandler)
-	port := ":8080"
-	http.ListenAndServe(port, nil)
+	r := router.CreateRoute()
+	r.Run(":8080")
 }
